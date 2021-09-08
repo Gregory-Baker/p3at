929ff233bf521fdb2e8930f94cb4b481c4a12d50
Gregory-Baker <gregory.baker@brl.ac.uk>
2021-09-08 16:28:29 +0000

    rosserial node not working

--- p3at_embedded/p3at_gripper_arduino/p3at_gripper_arduino.ino
+++ p3at_embedded/p3at_gripper_arduino/p3at_gripper_arduino.ino
@@ -5,17 +5,39 @@ Author: Greg Baker
 
 ******************************************************************************/
 
+#include "Gripper.h"
 #include <ros.h>
 #include <p3at_gripper/Command.h>
 
-void setup()
-{
-  
+p3at_gripper::Command cmd;
+
+Gripper gripper;
+
+ros::NodeHandle nh;
+
+void messageCb( const p3at_gripper::Command &command_msg){
+  if (command_msg.lift_command == 1) {
+    gripper.Up();
+  }
+  else if (command_msg.lift_command == -1) {
+    gripper.Down();
+  }
+  if (command_msg.gripper_command == 1) {
+    gripper.Open();
+  }
+  else if (command_msg.gripper_command == -1) {
+    gripper.Close();
+  } 
 }
 
+ros::Subscriber<p3at_gripper::Command> sub("gripper_command", &messageCb );
+
+void setup()
+{
+  nh.subscribe(sub);
+}
 
 void loop()
 {
-
-   
+  
 }
