ecfee3fb8344d7e8d28870db8b0bb83cee8fdf8a
Gregory-Baker <gregory.baker@brl.ac.uk>
2021-10-08 10:13:53 +0100

    simplified baud rate setting for embedded sketches and removed joy_teleop from base.launch

--- p3at_embedded/p3at_gripper_arduino/p3at_gripper_arduino.ino
+++ p3at_embedded/p3at_gripper_arduino/p3at_gripper_arduino.ino
@@ -5,30 +5,65 @@ Author: Greg Baker
 
 ******************************************************************************/
 
-#include "Gripper.h"
 #include <ros.h>
+#include "Gripper.h"
 #include <p3at_gripper/Command.h>
+#include <p3at_gripper/Feedback.h>
 
 p3at_gripper::Command cmd;
+p3at_gripper::Feedback feedback;
+ros::Publisher pub("gripper_feedback", &feedback);
 
 Gripper gripper;
 
 ros::NodeHandle nh;
 
+unsigned long cmdTime;
+unsigned long pubTime;
+unsigned long pubInterval = 100;
+
 void messageCb( const p3at_gripper::Command &command_msg){
+  cmdTime = millis();
   gripper.setCommand(command_msg.lift_command, command_msg.gripper_command);
 }
 
 ros::Subscriber<p3at_gripper::Command> sub("gripper_command", &messageCb );
 
+bool readings[6] = {false, false, false, false, false, false};
+
 void setup()
 {
+  cmdTime = millis();
+  //nh.getHardware()->setBaud(115200);
   nh.initNode();
   nh.subscribe(sub);
+  nh.advertise(pub);
 }
 
 void loop()
 {
-  gripper.actionCommand();
   nh.spinOnce();
+
+  unsigned long timeSinceCmd = millis() - cmdTime;
+  if (timeSinceCmd > 500) {
+    gripper.Stop();
+    feedback.gripper_command_received = false; 
+  }
+  else {
+    feedback.gripper_command_received = true;
+  }
+  gripper.actionCommand();
+  
+  if ((millis() - pubTime) > pubInterval) {
+    gripper.getRead(readings);
+    feedback.gripper_open = readings[0];
+    feedback.gripper_closed = readings[1];
+    feedback.gripper_lift_up = readings[2];
+    feedback.gripper_lift_down = readings[3];
+    feedback.gripper_ir_outer = readings[4];
+    feedback.gripper_ir_inner = readings[5];
+    feedback.header.stamp = nh.now();
+    pub.publish(&feedback);
+    pubTime = millis();
+  }
 }
